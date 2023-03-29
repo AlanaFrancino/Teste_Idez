73b8c122f321cfe3cba200561ae60a927c2febf9
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2023-03-29 14:57:02 -0300

    perf(MunicipiosController/municipiosPorUf): Add uso de cache

--- app/Http/Controllers/MunicipiosController.php
+++ app/Http/Controllers/MunicipiosController.php
@@ -3,24 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class MunicipiosController extends Controller
 {
     public function municipiosPorUf($uf)
     {
-        $apiUrl = env('API_PROVIDER') === 'BRASILAPI' ?
-            "https://brasilapi.com.br/api/ibge/municipios/v1/$uf" :
-            "https://servicodados.ibge.gov.br/api/v1/localidades/estados/$uf/municipios";
+        $cacheKey = 'municipios-' . $uf;
 
-        $response = Http::get($apiUrl);
-        $municipios = collect($response->json())->map(function ($municipio) {
-            return [
-                'name' => $municipio['nome'],
-                'ibge_code' => $municipio['ibge'] ?? null,
-            ];
+        $municipios = Cache::remember($cacheKey, 3600, function () use ($uf) {
+
+            $apiUrl = env('API_PROVIDER') === 'BRASILAPI' ?
+                "https://brasilapi.com.br/api/ibge/municipios/v1/$uf" :
+                "https://servicodados.ibge.gov.br/api/v1/localidades/estados/$uf/municipios";
+
+            $response = Http::get($apiUrl);
+            $municipios = collect($response->json())->map(function ($municipio) {
+                    return [
+                        'name' => $municipio['nome'],
+                        'ibge_code' => $municipio['ibge'] ?? null,
+                    ];
+            });
+
+            return response()->json($municipios);
         });
 
         return response()->json($municipios);
     }
+
 }
